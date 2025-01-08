f2ac1f480fc737d050a7477772e288087e854fb7
thelite5419 <prathameshpise6@gmail.com>
2025-01-08 15:08:17 +0530

    project file

--- 03BackendProjectSetup/src/db/index.js
+++ 03BackendProjectSetup/src/db/index.js
@@ -3,12 +3,12 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-       const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-       console.log(`\n the DB is successsfully connected !! ${connectionInstance.connection.host}`);     
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log(`DB successfully connected to: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log('DB conenction error:>', error)
-        process.exit(1)
+        console.error('DB connection error:>', error.message);
+        process.exit(1);
     }
-}
+};
 
-export default connectDB
+export default connectDB;
---
+++ 03BackendProjectSetup/src/models/user.models.js
@@ -0,0 +1,89 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+    {
+        username : {
+            type : String, 
+            required: true, 
+            lowercase: true, 
+            unique: true, 
+            trim: true, 
+            index: true
+        },
+        email : {
+            type : String, 
+            required: true, 
+            lowercase: true, 
+            unique: true, 
+            trim: true, 
+        },
+        fullname : {
+            type : String, 
+            required: true,
+            trim: true, 
+            index: true
+        },
+        avatar : {
+            type : String, 
+            required: true
+        },
+        coverimage : {
+            type : String, 
+        },
+        watchHistory : 
+            [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: "Video"
+                }
+            ],
+        password : {
+            type : String, 
+            required: [true, "password must required"]
+        },
+        refreshToken : {
+            type : String, 
+        },
+        
+    }, 
+    {timestamps: true}
+)
+
+
+userSchema.pre("save",async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+   return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function (params) {
+    jwt.sign({
+        _id: this._id, 
+        email: this.email,
+        username: this.username, 
+        fullname: this.fullname
+    }, 
+    process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+userSchema.methods.generateRefreshToken = function (params) {
+    jwt.sign({
+        _id: this._id, 
+    }, 
+    process.env.REFRESH_TOKEN_SECRET,{
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
+export const User = mongoose("User", userSchema)
